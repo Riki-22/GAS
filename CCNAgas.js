ead50b95c119ff8a5cd61d5acf43d452468fa2b5
Riki Ogasawara <>
2021-04-04 21:32:30 +0900

    変数var→letに修正

--- CCNAgas.js
+++ CCNAgas.js
@@ -4,35 +4,25 @@ const inputSheet = 'テーブル'; // データテーブルのシート名
 const outputSheet = 'テスト作成';　//　出力先のシート名
 const imageFolderId = '1Tbd4EPXxSxyIirGY8S7mMmPCEyg_GUdN'; // 画像が保存されているフォルダID
 const formFolderId = '1-C23Mbz4Q7IvRpocpmL39kRRROiTLJvv'; // 移動先のフォルダID
-var mail;
-var title;
-var description;
-var section;
-var random;
-var maxItem;
 
-function setResponses(e) {
 
-  mail = e.response.getRespondentEmail();
+// 必ずインストーラブルトリガーをrunに指定すること
+function run(e) {
+
+  let mailAddress = e.response.getRespondentEmail();
   let itemResponses = e.response.getItemResponses();
-  title = itemResponses[0].getResponse();
-  description = itemResponses[1].getResponse();
-  section = itemResponses[2].getResponse();
-  random = itemResponses[3].getResponse();
-  maxItem = itemResponses[4].getResponse();
+  let title = itemResponses[0].getResponse();
+  let description = itemResponses[1].getResponse();
+  let section = itemResponses[2].getResponse();
+  let random = itemResponses[3].getResponse();
+  let maxItem = itemResponses[4].getResponse();
 
-  run();
-}
-
-function run() {
-  
-  let data = getData(1, 2);
-  let form = createForm(title, description, data);
+  let data = getData(section, 1, 2); // データを取得するセルの開始位置(sectionカラムの最初のレコード)を入力
+  let form = createForm(title, description, data, maxItem);
   
   moveForm(form);
-  sendMail(form);
+  sendMail(mailAddress, form);
 }
-
 
 /**
  * getData
@@ -43,7 +33,7 @@ function run() {
  * @param {startCol:int} 問題文と選択肢が格納されている先頭の列番号
  * @return {array} 問題文と選択肢の２次元配列 (指定されたセルから最終行、最終列までが2次元配列として返される)
  */
-function getData(startRow, startCol) {
+function getData(section, startRow, startCol) {
   
   let sheet = SpreadsheetApp.openById(sheetId).getSheetByName(outputSheet);
   let colNameRow = '=index(\'' + inputSheet + '\'!A1:N1)';
@@ -68,7 +58,7 @@ function getData(startRow, startCol) {
  * @param {colName:array} 選択肢a~fを格納
  * @return {form} 生成されたGoogleフォーム(オブジェクト)
  */
-function createForm(title, description, data) {
+function createForm(title, description, data, maxItem) {
   
   let doc = DriveApp.getFileById(formId);
   let file = doc.makeCopy(title);
@@ -99,8 +89,8 @@ function createForm(title, description, data) {
     var qa = data[i];
     var questionNum = qa[0] + '-' + qa[1];
     var imageName = questionNum + '.png';  // ※正しい拡張子をつけること
-
     var fol = DriveApp.getFolderById(imageFolderId);
+
     if (fol.getFilesByName(imageName).hasNext()) {
       
       var blob = fol.getFilesByName(imageName).next().getBlob();
@@ -110,11 +100,11 @@ function createForm(title, description, data) {
         .setTitle(questionNum + '：図を参照して次の設問に回答してください。')
     }
 
-
     var answer = qa[qa.length - 2];
     let comment = qa[qa.length - 1];
     var choices = [];
     var choice = colName.slice(3, 9);
+
     if (answer.length == 1) {
 
       var item = form.addMultipleChoiceItem();
@@ -136,7 +126,6 @@ function createForm(title, description, data) {
     } else {
 
       var item = form.addCheckboxItem();
-
       var answers = answer.split(',');
       
       item.setTitle(questionNum + '：' + qa[2]);
@@ -178,9 +167,9 @@ function moveForm(form) {
   createdForm.moveTo(folder);
 }
 
-function sendMail(form) {
+function sendMail(mailAddress, form) {
 
   let subject = 'テスト送信';
   let body = '公開用 URL: ' + form.getPublishedUrl() + '\n編集用 URL: ' + form.getEditUrl();
-  GmailApp.sendEmail(mail, subject, body);
+  GmailApp.sendEmail(mailAddress, subject, body);
 }
